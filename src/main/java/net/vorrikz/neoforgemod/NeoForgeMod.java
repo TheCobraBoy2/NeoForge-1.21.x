39b94ca7b86456a681db4b540c36de5aac411387
TheCobraBoy2 <landoncs10@gmail.com>
2025-11-06 23:14:58 -0600

    config and removing some nonsense

---
+++ src/main/java/net/vorrikz/neoforgemod/NeoForgeMod.java
@@ -0,0 +1,42 @@
+package net.vorrikz.neoforgemod;
+
+import org.slf4j.Logger;
+
+import com.mojang.logging.LogUtils;
+
+import net.neoforged.bus.api.IEventBus;
+import net.neoforged.bus.api.SubscribeEvent;
+import net.neoforged.fml.common.Mod;
+import net.neoforged.fml.config.ModConfig;
+import net.neoforged.fml.ModContainer;
+import net.neoforged.fml.event.lifecycle.FMLCommonSetupEvent;
+import net.neoforged.neoforge.common.NeoForge;
+import net.neoforged.neoforge.event.BuildCreativeModeTabContentsEvent;
+import net.neoforged.neoforge.event.server.ServerStartingEvent;
+
+// The value here should match an entry in the META-INF/neoforge.mods.toml file
+@Mod(NeoForgeMod.MOD_ID)
+public class NeoForgeMod {
+    public static final String MOD_ID = "vorrikzneoforgemod";
+    public static final Logger LOGGER = LogUtils.getLogger();
+
+    public NeoForgeMod(IEventBus modEventBus, ModContainer modContainer) {
+        modEventBus.addListener(this::commonSetup);
+
+        NeoForge.EVENT_BUS.register(this);
+
+        modEventBus.addListener(this::addCreative);
+
+        modContainer.registerConfig(ModConfig.Type.COMMON, Config.SPEC);
+    }
+
+    private void commonSetup(FMLCommonSetupEvent event) {
+    }
+
+    private void addCreative(BuildCreativeModeTabContentsEvent event) {
+    }
+
+    @SubscribeEvent
+    public void onServerStarting(ServerStartingEvent event) {
+    }
+}
